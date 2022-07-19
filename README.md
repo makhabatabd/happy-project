770016a2024173b3dc9ed090eade447208d3061c
makhabatabd <makhabat9898@gmail.com>
2022-07-19 14:13:29 +0600

    done with verstka and mode changes

---
+++ README.md
@@ -0,0 +1,23 @@
+# Shop
+
+## Run :
+
+```.bash
+ git clone https://github.com/makhabatabd/happy-project.git
+
+    npm install
+```
+
+### After initialization the project - run:
+
+```.bash
+    npm run start
+```
+
+### or
+
+```.bash
+    npm run build
+```
+
+### to get (dist) folder, which containts all files to run site.
--- src/index.js
+++ src/index.js
@@ -1,2 +1,29 @@
 import "bootstrap";
 import "./styles/styles.scss";
+
+const light_div = document.getElementById("light__div");
+const dark_div = document.getElementById("dark__div");
+const light = document.getElementById("light");
+const dark = document.getElementById("dark");
+const mode = document.getElementById("mode");
+
+light_div.addEventListener("click", () => {
+  mode.classList.add("light__mode");
+  mode.classList.remove("dark__mode");
+  if (mode.classList.contains("light__mode")) {
+    light.classList.remove("white");
+    light.classList.add("orange");
+    dark.classList.remove("orange");
+    dark.classList.add("black");
+  }
+});
+
+dark_div.addEventListener("click", () => {
+  mode.classList.add("dark__mode");
+  if (mode.classList.contains("dark__mode")) {
+    light.classList.remove("orange");
+    light.classList.add("white");
+    dark.classList.remove("black");
+    dark.classList.add("orange");
+  }
+});
