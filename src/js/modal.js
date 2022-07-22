4206907f50332be506126604c2079424e14adb38
makhabatabd <makhabat9898@gmail.com>
2022-07-22 12:52:30 +0600

    modal changes

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,99 +1,3 @@
-// const modal = document.getElementById("modal");
-// const cheap = document.getElementById("cheap");
-// const medium = document.getElementById("medium");
-// const expensive = document.getElementById("expensive");
-// const buy = document.getElementById("buy");
-// const close = document.getElementById("close");
-// const standard = document.getElementById("standard");
-// const premium = document.getElementById("premium");
-// const lifetime = document.getElementById("lifetime");
-// const send = document.getElementById("send");
-// const name = document.getElementById("name");
-// const email = document.getElementById("email");
-// const form = document.getElementById("form");
-// const loading = document.getElementById("loading");
-// const outter = document.querySelector(".modal__outter");
-// const name_label = document.querySelector(".name__label");
-// const email_label = document.querySelector(".email__label");
-
-// cheap.addEventListener("click", () => {
-//   modal.style.display = "block";
-//   document.body.style.overflow = "hidden";
-//   outter.style.display = "block";
-//   standard.classList.add("cheap");
-// });
-
-// medium.addEventListener("click", () => {
-//   modal.style.display = "block";
-//   document.body.style.overflow = "hidden";
-//   outter.style.display = "block";
-//   premium.classList.add("medium");
-// });
-
-// expensive.addEventListener("click", () => {
-//   modal.style.display = "block";
-//   document.body.style.overflow = "hidden";
-//   outter.style.display = "block";
-//   lifetime.classList.add("expensive");
-// });
-
-// buy.addEventListener("click", () => {
-//   modal.style.display = "block";
-//   document.body.style.overflow = "hidden";
-//   outter.style.display = "block";
-//   lifetime.classList.add("expensive");
-// });
-
-// close.addEventListener("click", (e) => {
-//   e.preventDefault();
-//   modal.style.display = "none";
-//   outter.style.display = "none";
-//   standard.classList.remove("cheap");
-//   premium.classList.remove("medium");
-//   lifetime.classList.remove("expensive");
-//   loading.style.display = "none";
-//   document.body.style.overflow = "auto";
-//   form.reset();
-//   name_label.style.display = "none";
-//   email_label.style.display = "none";
-// });
-
-// close.addEventListener("click", (e) => {
-//   e.preventDefault();
-//   outter.style.display = "none";
-//   modal.style.display = "block";
-//   standard.classList.remove("cheap");
-//   premium.classList.remove("medium");
-//   lifetime.classList.remove("expensive");
-//   loading.style.display = "none";
-//   document.body.style.overflow = "auto";
-//   form.reset();
-//   name_label.style.display = "none";
-//   email_label.style.display = "none";
-// });
-
-// send.addEventListener("click", (e) => {
-//   e.preventDefault();
-//   name.value === ""
-//     ? (name_label.style.display = "block")
-//     : (name_label.style.display = "none");
-//   email.value === ""
-//     ? (email_label.style.display = "block")
-//     : (email_label.style.display = "none");
-//   if (name.value !== "" && email.value !== "") {
-//     loading.style.display = "block";
-//     setTimeout(function () {
-//       outter.style.display = "none";
-//       standard.classList.remove("cheap");
-//       premium.classList.remove("medium");
-//       lifetime.classList.remove("expensive");
-//       loading.style.display = "none";
-//       document.body.style.overflow = "auto";
-//       form.reset();
-//     }, 3000);
-//   }
-// });
-
 const buttons = document.querySelectorAll(".option__button");
 const inputs = document.querySelectorAll(".modal-plan__radio");
 const modal = document.getElementById("modal");
@@ -112,17 +16,35 @@ let plan;
 buttons.forEach((item) =>
   item.addEventListener("click", (e) => {
     e.preventDefault();
-    // document.body.style.overflow = "hidden";
+    document.body.style.overflow = "hidden";
     plan = e.target.value;
     modal.style.display = "block";
     outter.style.display = "block";
-
-    console.log(inputs);
     inputs.forEach((item) =>
       item.value == plan ? (item.checked = true) : (item.checked = false)
     );
   })
 );
+
+function changeName() {
+  name_label.style.display = "block";
+  name.style.backgroundColor = "#FFEEEE";
+}
+
+function changeEmail() {
+  email_label.style.display = "block";
+  email.style.backgroundColor = "#FFEEEE";
+}
+
+function undoName() {
+  name_label.style.display = "none";
+  name.style.backgroundColor = "#F2F2F2";
+}
+
+function undoEmail() {
+  email_label.style.display = "none";
+  email.style.backgroundColor = "#F2F2F2";
+}
 
 close.addEventListener("click", (e) => {
   e.preventDefault();
@@ -131,20 +53,22 @@ close.addEventListener("click", (e) => {
   loading.style.display = "none";
   document.body.style.overflow = "auto";
   form.reset();
-  name_label.style.display = "none";
-  email_label.style.display = "none";
+  undoEmail();
+  undoName();
 });
 
 send.addEventListener("click", (e) => {
   e.preventDefault();
-  name.value === ""
-    ? (name_label.style.display = "block")
-    : (name_label.style.display = "none");
-  email.value === ""
-    ? (email_label.style.display = "block")
-    : (email_label.style.display = "none");
+  if (name.value === "") {
+    changeName();
+  }
+  if (email.value === "") {
+    changeEmail();
+  }
   if (name.value !== "" && email.value !== "") {
     loading.style.display = "block";
+    undoEmail();
+    undoName();
     setTimeout(function () {
       outter.style.display = "none";
       loading.style.display = "none";
@@ -152,4 +76,12 @@ send.addEventListener("click", (e) => {
       form.reset();
     }, 3000);
   }
+});
+
+name.addEventListener("input", () => {
+  name.value !== "" ? undoName() : changeName();
+});
+
+email.addEventListener("input", () => {
+  email.value !== "" ? undoEmail() : changeEmail();
 });
